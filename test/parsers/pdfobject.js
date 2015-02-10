fc9e5a3530fe7c208a281d3e521fee947f00e412
Christopher Brown <io@henrian.com>
2015-02-10 11:35:45 -0600

    Add pdfobject parser tests

---
+++ test/parsers/pdfobject.js
@@ -0,0 +1,95 @@
+var assert = require('assert');
+//var js_yaml = require('js-yaml');
+//var translations = js_yaml.safeLoad(fs.readFileSync(path.join(__dirname, 'pdfobject.yaml'), {encoding: 'utf8'}));
+var parser = require('../../parsers/pdfobject');
+function check(input, expected_output) {
+    var output = parser.parse(input);
+    assert.deepEqual(output, expected_output);
+}
+describe('pdfobject parser', function () {
+    it('should parse short binary string', function () {
+        var input = "<ea68d4>";
+        var output = [234, 104, 212];
+        assert.deepEqual(parser.parse(input), output);
+    });
+    it('should parse dictionary object with indirect references', function () {
+        var input = "<<\n/Size 369\n/Info 339 0 R\n/Root 342 0 R\n/Prev 632196\n/ID[<7e19ea68d47cd58418bb9001776e808b><7e19ea68d47cd58418bb9001776e808b>]\n>>";
+        var output = {
+            Size: 369,
+            Info: {
+                object_number: 339,
+                generation_number: 0,
+            },
+            Root: {
+                object_number: 342,
+                generation_number: 0,
+            },
+            Prev: 632196,
+            ID: [
+                [126, 25, 234, 104, 212, 124, 213, 132, 24, 187, 144, 1, 119, 110, 128, 139],
+                [126, 25, 234, 104, 212, 124, 213, 132, 24, 187, 144, 1, 119, 110, 128, 139],
+            ]
+        };
+        assert.deepEqual(parser.parse(input), output);
+    });
+    it('should parse simple dictionary object', function () {
+        var input = "<<\n/Size 369\n/Info 339\n/Root 342\n/Prev 632196\n/ID (7e19 808b)\n>>";
+        check(input, {
+            Size: 369,
+            Info: 339,
+            Root: 342,
+            Prev: 632196,
+            ID: "7e19 808b",
+        });
+    });
+    it('should parse real dictionary object', function () {
+        var input = "<< /Author (Kenneth Ward Church) /CreationDate (D:20020326140046-05'00') /ModDate (D:20020403103951-05'00') /Title (Char align: A Program for Aligning Parallel Texts at the Character Level) >>";
+        check(input, {
+            Author: 'Kenneth Ward Church',
+            CreationDate: "D:20020326140046-05'00'",
+            ModDate: "D:20020403103951-05'00'",
+            Title: 'Char align: A Program for Aligning Parallel Texts at the Character Level'
+        });
+    });
+    it('should parse nested dictionary object', function () {
+        var input = "<<\n/Fields [ ]\n/DR << /Font << /ZaDb 316 0 R /Helv 317 0 R >> /Encoding << /PDFDocEncoding 318 0 R >> >>\n/DA (/Helv 0 Tf 0 g )\n>>";
+        var output = {
+            Fields: [],
+            DR: {
+                Font: {
+                    ZaDb: {
+                        object_number: 316,
+                        generation_number: 0,
+                    },
+                    Helv: {
+                        object_number: 317,
+                        generation_number: 0,
+                    },
+                },
+                Encoding: {
+                    PDFDocEncoding: {
+                        object_number: 318,
+                        generation_number: 0,
+                    },
+                }
+            },
+            DA: "/Helv 0 Tf 0 g ",
+        };
+        assert.deepEqual(parser.parse(input), output);
+    });
+    it('should parse array of names', function () {
+        var input = "[\n/PDF /Text /ImageB\n]";
+        var output = ['PDF', 'Text', 'ImageB'];
+        assert.deepEqual(parser.parse(input), output);
+    });
+    it('should parse array of references', function () {
+        var input = "[\n4 0 R 6 0 R 8 0 R 10 0 R\n]";
+        var output = [
+            { object_number: 4, generation_number: 0 },
+            { object_number: 6, generation_number: 0 },
+            { object_number: 8, generation_number: 0 },
+            { object_number: 10, generation_number: 0 },
+        ];
+        assert.deepEqual(parser.parse(input), output);
+    });
+});
---
+++ test/parsers/pdfobject.ts
@@ -0,0 +1,129 @@
+/// <reference path="../../type_declarations/index.d.ts" />
+import fs = require('fs');
+import path = require('path');
+import assert = require('assert');
+
+//var js_yaml = require('js-yaml');
+//var translations = js_yaml.safeLoad(fs.readFileSync(path.join(__dirname, 'pdfobject.yaml'), {encoding: 'utf8'}));
+
+var parser = require('../../parsers/pdfobject');
+
+function check(input, expected_output) {
+  var output = parser.parse(input);
+  assert.deepEqual(output, expected_output);
+}
+
+describe('pdfobject parser', function() {
+  it('should parse short binary string', function() {
+    var input = `<ea68d4>`;
+    var output = [234, 104, 212];
+    assert.deepEqual(parser.parse(input), output);
+  });
+
+  it('should parse dictionary object with indirect references', function() {
+    var input = `<<
+/Size 369
+/Info 339 0 R
+/Root 342 0 R
+/Prev 632196
+/ID[<7e19ea68d47cd58418bb9001776e808b><7e19ea68d47cd58418bb9001776e808b>]
+>>`;
+    var output = {
+      Size: 369,
+      Info: {
+        object_number: 339,
+        generation_number: 0,
+      },
+      Root: {
+        object_number: 342,
+        generation_number: 0,
+      },
+      Prev: 632196,
+      ID: [
+        [126,25,234,104,212,124,213,132,24,187,144,1,119,110,128,139],
+        [126,25,234,104,212,124,213,132,24,187,144,1,119,110,128,139],
+      ]
+    };
+    assert.deepEqual(parser.parse(input), output);
+  });
+
+  it('should parse simple dictionary object', function() {
+    var input = `<<
+/Size 369
+/Info 339
+/Root 342
+/Prev 632196
+/ID (7e19 808b)
+>>`;
+    check(input, {
+      Size: 369,
+      Info: 339,
+      Root: 342,
+      Prev: 632196,
+      ID: "7e19 808b",
+    });
+  });
+
+  it('should parse real dictionary object', function() {
+    var input = `<< /Author (Kenneth Ward Church) /CreationDate (D:20020326140046-05'00') /ModDate (D:20020403103951-05'00') /Title (Char align: A Program for Aligning Parallel Texts at the Character Level) >>`;
+    check(input, {
+      Author: 'Kenneth Ward Church',
+      CreationDate: "D:20020326140046-05'00'",
+      ModDate: "D:20020403103951-05'00'",
+      Title: 'Char align: A Program for Aligning Parallel Texts at the Character Level'
+    });
+  });
+
+  it('should parse nested dictionary object', function() {
+    var input = `<<
+/Fields [ ]
+/DR << /Font << /ZaDb 316 0 R /Helv 317 0 R >> /Encoding << /PDFDocEncoding 318 0 R >> >>
+/DA (/Helv 0 Tf 0 g )
+>>`;
+    var output = {
+      Fields: [],
+      DR: {
+        Font: {
+          ZaDb: {
+            object_number: 316,
+            generation_number: 0,
+          },
+          Helv: {
+            object_number: 317,
+            generation_number: 0,
+          },
+        },
+        Encoding: {
+          PDFDocEncoding: {
+            object_number: 318,
+            generation_number: 0,
+          },
+        }
+      },
+      DA: "/Helv 0 Tf 0 g ",
+    };
+    assert.deepEqual(parser.parse(input), output);
+  });
+
+  it('should parse array of names', function() {
+    var input = `[
+/PDF /Text /ImageB
+]`;
+    var output = ['PDF', 'Text', 'ImageB'];
+    assert.deepEqual(parser.parse(input), output);
+  });
+
+  it('should parse array of references', function() {
+    var input = `[
+4 0 R 6 0 R 8 0 R 10 0 R
+]`;
+    var output = [
+      { object_number:  4, generation_number: 0 },
+      { object_number:  6, generation_number: 0 },
+      { object_number:  8, generation_number: 0 },
+      { object_number: 10, generation_number: 0 },
+    ]
+    assert.deepEqual(parser.parse(input), output);
+  });
+
+});
