6e719dd7b0df125109814a87d562f0217e44c7e4
Christopher Brown <io@henrian.com>
2015-02-09 20:02:28 -0600

    Add Buffer iteration helpers: bufferops.{compare,indexOf,equalTo}

---
+++ bufferops.js
@@ -0,0 +1,62 @@
+/// <reference path="type_declarations/index.d.ts" />
+/** bufferops#compare(haystack: Buffer, needle: Buffer, ...): boolean
+ *
+ * returns true if haystack starts with needle, i.e.:
+ *
+ *     haystack[haystack_offset:haystack_length] == needle[:needle_length]
+ *
+ * Effectively bufferops#startsWith if only haystack and needle are given.
+ */
+function compare(haystack, needle, haystack_offset, haystack_length, needle_length) {
+    if (haystack_offset === void 0) { haystack_offset = 0; }
+    if (haystack_length === void 0) { haystack_length = haystack.length; }
+    if (needle_length === void 0) { needle_length = needle.length; }
+    if ((haystack_offset + needle_length) > haystack_length)
+        return false;
+    for (var i = 0; i < needle_length; i++) {
+        if (needle[i] !== haystack[haystack_offset + i]) {
+            return false;
+        }
+    }
+    return true;
+}
+exports.compare = compare;
+/** bufferops#indexOf(haystack: Buffer, needle: Buffer, ...): number
+ *
+ * Returns the first index of needle within haystack that's greater than or
+ * equal to haystack_offset.
+ * Returns null if haystack does not contain needle.
+ */
+function indexOf(haystack, needle, haystack_offset, haystack_length, needle_length) {
+    if (haystack_offset === void 0) { haystack_offset = 0; }
+    if (haystack_length === void 0) { haystack_length = haystack.length; }
+    if (needle_length === void 0) { needle_length = needle.length; }
+    for (var i = haystack_offset; i < haystack_length; i++) {
+        if (compare(haystack, needle, i, haystack_length, needle_length)) {
+            return i;
+        }
+    }
+    return null;
+}
+exports.indexOf = indexOf;
+/** bufferops#equalTo(left: Buffer, right: Buffer, ...): boolean
+ * Returns true iff the designated slices of left and right are equal.
+ *   left[left_offset:left_length] == right[right_offset:right_length]
+ */
+function equalTo(left, right, left_offset, left_end, right_offset, right_end) {
+    if (left_offset === void 0) { left_offset = 0; }
+    if (left_end === void 0) { left_end = left.length - left_offset; }
+    if (right_offset === void 0) { right_offset = 0; }
+    if (right_end === void 0) { right_end = right.length - right_offset; }
+    var left_length = left_end - left_offset;
+    // return false immediately if they are different lengths
+    if (left_length !== right_end - right_offset)
+        return false;
+    for (var i = 0; i < left_length; i++) {
+        if (left[left_offset + i] !== right[right_offset + i]) {
+            return false;
+        }
+    }
+    return true;
+}
+exports.equalTo = equalTo;
---
+++ bufferops.ts
@@ -0,0 +1,63 @@
+/// <reference path="type_declarations/index.d.ts" />
+
+/** bufferops#compare(haystack: Buffer, needle: Buffer, ...): boolean
+ *
+ * returns true if haystack starts with needle, i.e.:
+ *
+ *     haystack[haystack_offset:haystack_length] == needle[:needle_length]
+ *
+ * Effectively bufferops#startsWith if only haystack and needle are given.
+ */
+export function compare(haystack: Buffer,
+                        needle: Buffer,
+                        haystack_offset: number = 0,
+                        haystack_length: number = haystack.length,
+                        needle_length: number = needle.length) {
+  if ((haystack_offset + needle_length) > haystack_length) return false;
+  for (var i = 0; i < needle_length; i++) {
+    if (needle[i] !== haystack[haystack_offset + i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+/** bufferops#indexOf(haystack: Buffer, needle: Buffer, ...): number
+ *
+ * Returns the first index of needle within haystack that's greater than or
+ * equal to haystack_offset.
+ * Returns null if haystack does not contain needle.
+ */
+export function indexOf(haystack: Buffer,
+                        needle: Buffer,
+                        haystack_offset: number = 0,
+                        haystack_length: number = haystack.length,
+                        needle_length: number = needle.length) {
+  for (var i = haystack_offset; i < haystack_length; i++) {
+    if (compare(haystack, needle, i, haystack_length, needle_length)) {
+      return i;
+    }
+  }
+  return null;
+}
+
+/** bufferops#equalTo(left: Buffer, right: Buffer, ...): boolean
+ * Returns true iff the designated slices of left and right are equal.
+ *   left[left_offset:left_length] == right[right_offset:right_length]
+ */
+export function equalTo(left: Buffer, right: Buffer,
+                        left_offset: number = 0,
+                        left_end: number = left.length - left_offset,
+                        right_offset: number = 0,
+                        right_end: number = right.length - right_offset) {
+  var left_length = left_end - left_offset;
+  // return false immediately if they are different lengths
+  if (left_length !== right_end - right_offset) return false;
+  // check each character
+  for (var i = 0; i < left_length; i++) {
+    if (left[left_offset + i] !== right[right_offset + i]) {
+      return false;
+    }
+  }
+  return true;
+}
