68451b13cbcc5c76d9454b3366849a4391bc0273
Christopher Brown <io@henrian.com>
2015-02-22 17:38:18 -0600

    Fix xref strings in xref test suite to be PDF compliant; add examples from PDF spec as new tests; improve check() error message

--- test/parsers/xref.js
+++ test/parsers/xref.js
@@ -3,11 +3,12 @@ var assert = require('assert');
 var parser = require('../../parsers/xref');
 function check(input, expected_output) {
     var output = parser.parse(input);
-    assert.deepEqual(output, expected_output);
+    var message = "parse result does not match expected output.\n      parse(\"" + input + "\") => " + JSON.stringify(output) + "\n      but should == " + JSON.stringify(expected_output);
+    assert.deepEqual(output, expected_output, message);
 }
 describe('pdfobject parser', function () {
     it('short xref with trailing newline', function () {
-        var input = "xref\n0 2\n0000000000 65535 f\n0000000015 00000 n\n";
+        var input = "xref\n0 2\n0000000000 65535 f\r\n0000000015 00000 n\r\n";
         var output = [
             {
                 object_number: 0,
@@ -24,8 +25,8 @@ describe('pdfobject parser', function () {
         ];
         check(input, output);
     });
-    it('short xref without trailing newline', function () {
-        var input = "xref\n100 3\n0000000197 00000 n\n0000000556 00000 n\n0001000023 00000 n";
+    it('another xref with trailing newline', function () {
+        var input = "xref\n100 3\n0000000197 00000 n\r\n0000000556 00000 n\r\n0001000023 00000 n\r\n";
         var output = [
             {
                 object_number: 100,
@@ -44,6 +45,84 @@ describe('pdfobject parser', function () {
                 offset: 1000023,
                 generation_number: 0,
                 in_use: true
+            }
+        ];
+        check(input, output);
+    });
+    it('xref from PDF32000_2008.pdf Section 7.5.4 EXAMPLE 2', function () {
+        var input = "xref\n0 6\n0000000003 65535 f\r\n0000000017 00000 n\r\n0000000081 00000 n\r\n0000000000 00007 f\r\n0000000331 00000 n\r\n0000000409 00000 n\r\n";
+        var output = [
+            {
+                "offset": 3,
+                "generation_number": 65535,
+                "in_use": false,
+                "object_number": 0
+            },
+            {
+                "offset": 17,
+                "generation_number": 0,
+                "in_use": true,
+                "object_number": 1
+            },
+            {
+                "offset": 81,
+                "generation_number": 0,
+                "in_use": true,
+                "object_number": 2
+            },
+            {
+                "offset": 0,
+                "generation_number": 7,
+                "in_use": false,
+                "object_number": 3
+            },
+            {
+                "offset": 331,
+                "generation_number": 0,
+                "in_use": true,
+                "object_number": 4
+            },
+            {
+                "offset": 409,
+                "generation_number": 0,
+                "in_use": true,
+                "object_number": 5
+            }
+        ];
+        check(input, output);
+    });
+    it('xref from PDF32000_2008.pdf Section 7.5.4 EXAMPLE 3', function () {
+        var input = "xref\n0 1\n0000000000 65535 f\r\n3 1\n0000025325 00000 n\r\n23 2\n0000025518 00002 n\r\n0000025635 00000 n\r\n30 1\n0000025777 00000 n\r\n";
+        var output = [
+            {
+                "object_number": 0,
+                "offset": 0,
+                "generation_number": 65535,
+                "in_use": false
+            },
+            {
+                "object_number": 3,
+                "offset": 25325,
+                "generation_number": 0,
+                "in_use": true
+            },
+            {
+                "object_number": 23,
+                "offset": 25518,
+                "generation_number": 2,
+                "in_use": true
+            },
+            {
+                "object_number": 24,
+                "offset": 25635,
+                "generation_number": 0,
+                "in_use": true
+            },
+            {
+                "object_number": 30,
+                "offset": 25777,
+                "generation_number": 0,
+                "in_use": true
             }
         ];
         check(input, output);
