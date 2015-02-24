8270bff6116cc6318a0009b00317bd80e9bd1c60
Christopher Brown <io@henrian.com>
2015-02-24 14:31:46 -0600

    Remove .parse overload in pdfobject module (only support BufferedReader)

--- parsers/pdfobject.js
+++ parsers/pdfobject.js
@@ -2,17 +2,17 @@
 var logger = require('loge');
 var chalk = require('chalk');
 var term = require('../dev/term');
-var File = require('../File');
-var BufferedFileReader = require('../readers/BufferedFileReader');
 var BufferedStringReader = require('../readers/BufferedStringReader');
 var BufferedLexer = require('./BufferedLexer');
-function printParseException(input, exception) {
-    logger.error('(%d,%d): [%d/%d] %s', exception.line, exception.column, exception.offset, input.length, exception.toString());
+function printParseException(reader, exception) {
+    console.error(chalk.red(exception.message));
+    return;
+    logger.error('(%d,%d): [%d/%d] %s', exception.line, exception.column, exception.offset, reader.length, exception.toString());
     // 32 4 1 [ { type: 'class', value: '[0-9]', description: '[0-9]' },
-    var margin = 256; // input.length > 256 ? 64 : 256;
-    var prefix = input.slice(Math.max(0, exception.offset - margin), exception.offset);
-    var position = input.slice(exception.offset, exception.offset + 1);
-    var postfix = input.slice(exception.offset + 1, exception.offset + margin);
+    var margin = 256; // reader.length > 256 ? 64 : 256;
+    var prefix = reader.slice(Math.max(0, exception.offset - margin), exception.offset);
+    var position = reader.slice(exception.offset, exception.offset + 1);
+    var postfix = reader.slice(exception.offset + 1, exception.offset + margin);
     term.print(term.escape(prefix) + chalk.bgRed(term.escape(position)) + term.escape(postfix));
     // exception.offset, exception.line, exception.column, exception.expected, exception.found);
     // term.print(exc.offset, exc.line, exc.column, exc.expected, exc.found, exc.name, Object.keys(exc));
@@ -20,24 +20,19 @@ function printParseException(input, exception) {
 // load the precompiled Jison parser
 var Parser = require('./pdfobject.parser').Parser;
 var pdfrules = require('./pdfrules');
-function createReader(input) {
-    if (input instanceof File) {
-        return new BufferedFileReader(input);
-    }
-    else {
-        return new BufferedStringReader(input);
-    }
+function parseString(input) {
+    var reader = new BufferedStringReader(input);
+    return parse(reader);
 }
-function parse(input) {
-    var reader = createReader(input);
+exports.parseString = parseString;
+function parse(reader) {
     var parser = new Parser();
     parser.lexer = new BufferedLexer(pdfrules);
     try {
         return parser.parse(reader);
     }
     catch (exc) {
-        // printParseException(reader, exc);
-        console.error(exc.message);
+        printParseException(reader, exc);
         throw exc;
     }
 }
--- parsers/pdfobject.ts
+++ parsers/pdfobject.ts
@@ -11,14 +11,16 @@ import BufferedStringReader = require('../readers/BufferedStringReader');
 
 import BufferedLexer = require('./BufferedLexer');
 
-function printParseException(input, exception) {
+function printParseException(reader, exception) {
+  console.error(chalk.red(exception.message));
+  return;
   logger.error('(%d,%d): [%d/%d] %s', exception.line, exception.column,
-    exception.offset, input.length, exception.toString());
+    exception.offset, reader.length, exception.toString());
   // 32 4 1 [ { type: 'class', value: '[0-9]', description: '[0-9]' },
-  var margin = 256; // input.length > 256 ? 64 : 256;
-  var prefix = input.slice(Math.max(0, exception.offset - margin), exception.offset);
-  var position = input.slice(exception.offset, exception.offset + 1)
-  var postfix = input.slice(exception.offset + 1, exception.offset + margin);
+  var margin = 256; // reader.length > 256 ? 64 : 256;
+  var prefix = reader.slice(Math.max(0, exception.offset - margin), exception.offset);
+  var position = reader.slice(exception.offset, exception.offset + 1)
+  var postfix = reader.slice(exception.offset + 1, exception.offset + margin);
   term.print(term.escape(prefix) + chalk.bgRed(term.escape(position)) + term.escape(postfix));
   // exception.offset, exception.line, exception.column, exception.expected, exception.found);
   // term.print(exc.offset, exc.line, exc.column, exc.expected, exc.found, exc.name, Object.keys(exc));
@@ -29,18 +31,12 @@ var Parser = require('./pdfobject.parser').Parser;
 
 var pdfrules = require('./pdfrules');
 
-function createReader(input: string | File): BufferedReader {
-  if (<any>input instanceof File) {
-    return new BufferedFileReader(<File>input);
-  }
-  else {
-    return new BufferedStringReader(<string>input);
-  }
+export function parseString(input: string): pdfdom.PDFObject {
+  var reader = new BufferedStringReader(input);
+  return parse(reader);
 }
 
-export function parse(input: string | File): pdfdom.PDFObject {
-  var reader = createReader(input);
-
+export function parse(reader: BufferedReader): pdfdom.PDFObject {
   var parser = new Parser();
   parser.lexer = new BufferedLexer(pdfrules);
 
@@ -48,8 +44,7 @@ export function parse(input: string | File): pdfdom.PDFObject {
     return parser.parse(reader);
   }
   catch (exc) {
-    // printParseException(reader, exc);
-    console.error(exc.message);
+    printParseException(reader, exc);
     throw exc;
   }
 }
