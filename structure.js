97a6137b85b0dac63d7e150afef5a9ef0b2e6e56
Christopher Brown <io@henrian.com>
2015-02-24 14:39:41 -0600

    Fix structure to match recent file / reader / parser changes & refactoring

--- structure.js
+++ structure.js
@@ -1,40 +1,9 @@
-var __extends = this.__extends || function (d, b) {
-    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
-    function __() { this.constructor = d; }
-    __.prototype = b.prototype;
-    d.prototype = new __();
-};
-/// <reference path="type_declarations/index.d.ts" />
-var logger = require('loge');
-var chalk = require('chalk');
-var FileCursor = require('./FileCursor');
+var File = require('./File');
+var FileReader = require('./readers/FileReader');
+var BufferedFileReader = require('./readers/BufferedFileReader');
 var term = require('./dev/term');
-function printParseException(input, exception) {
-    logger.error('(%d,%d): [%d/%d] %s', exception.line, exception.column, exception.offset, input.length, exception.toString());
-    // 32 4 1 [ { type: 'class', value: '[0-9]', description: '[0-9]' },
-    var margin = 256; // input.length > 256 ? 64 : 256;
-    var prefix = input.slice(Math.max(0, exception.offset - margin), exception.offset);
-    var position = input.slice(exception.offset, exception.offset + 1);
-    var postfix = input.slice(exception.offset + 1, exception.offset + margin);
-    term.print(term.escape(prefix) + chalk.bgRed(term.escape(position)) + term.escape(postfix));
-    // exception.offset, exception.line, exception.column, exception.expected, exception.found);
-    // term.print(exc.offset, exc.line, exc.column, exc.expected, exc.found, exc.name, Object.keys(exc));
-}
-function wrapParser(parser) {
-    return {
-        parse: function (input) {
-            try {
-                return parser.parse(input);
-            }
-            catch (exc) {
-                printParseException(input, exc);
-                throw exc;
-            }
-        }
-    };
-}
-var pdfobject_parser = wrapParser(require('./parsers/pdfobject'));
-var xref_parser = wrapParser(require('./parsers/xref'));
+var pdfobject_parser = require('./parsers/pdfobject');
+var xref_parser = require('./parsers/xref');
 /** parseStartXref(buffer)
  *
  * Read a snippet like
@@ -66,83 +35,77 @@ function findCrossReference(reference, cross_references) {
     }
     throw new Error("Could not find a cross reference for\n    " + reference.object_number + ":" + reference.generation_number);
 }
-var PDFFileReader = (function (_super) {
-    __extends(PDFFileReader, _super);
-    // the trailer will generally have two important fields: "Root" and "Info",
-    // both of which are object references
-    function PDFFileReader(filepath) {
-        _super.call(this, filepath);
-        this.filepath = filepath;
+/**
+Reads the final xref and trailer from the opened PDF file, returning a
+minimal pdfdom.PDF structure (just trailer and cross_references)
+
+The trailer will generally have two important fields: "Root" and "Info",
+both of which are object references.
+*/
+function readMetadata(reader) {
+    // 1. find the final "startxref <offset> %%EOF", which should happen in the last 64 bytes
+    var footer = reader.readRangeUntilString(reader.size - 64, '%%EOF');
+    if (footer == null)
+        throw new Error('Could not find final %%EOF');
+    var xref_offset = parseStartXref(footer.buffer);
+    // 2. seek to that xref and read until the trailer
+    var xref_content = reader.readRangeUntilString(xref_offset, 'trailer');
+    var xref_string = xref_content.buffer.toString('ascii');
+    // term.print('xref_string: %j', xref_string);
+    var cross_references = xref_parser.parse(xref_string);
+    // 3. read the trailer
+    var trailer_content = reader.readRangeUntilString(xref_content.end, 'startxref');
+    // skip over the "trailer" marker
+    var trailer_string = trailer_content.buffer.slice(7).toString('ascii').trim();
+    // term.print('trailer_string: %j', trailer_string);
+    var trailer_object = pdfobject_parser.parseString(trailer_string);
+    // term.print('trailer_object: %j', trailer_object);
+    var trailer = trailer_object;
+    //var next_xref = indexed_file.readRangeUntil(trailer_content.end, '%%EOF');
+    // next_xref is just the XREF from the first lookup
+    return {
+        cross_references: cross_references,
+        trailer: trailer,
+    };
+}
+/**
+Read a specific object from a PDF, given a desired reference and list of cross
+references.
+*/
+function findObject(file, reference, cross_references) {
+    var cross_reference = findCrossReference(reference, cross_references);
+    // TODO: only match endobj at the beginning of lines
+    // var object_content = reader.readRangeUntilString(cross_reference.offset, 'endobj');
+    // var object_string = object_content.buffer.toString('ascii');
+    var reader = new BufferedFileReader(file, cross_reference.offset);
+    var object = pdfobject_parser.parse(reader);
+    // object is a pdfdom.IndirectObject, but we already knew the object number
+    // and generation number; that's how we found it. We only want the value of
+    // the object. But we might as well double check that what we got is what
+    // we were looking for:
+    if (object.object_number != cross_reference.object_number) {
+        throw new Error("PDF cross references are incorrect; the offset\n      " + cross_reference.offset + " does not lead to an object numbered\n      " + cross_reference.object_number + "; instead, the object at that offset is\n      " + object.object_number);
     }
-    /** PDFFileReader#readMetadata()
-     * Reads the final xref and trailer from the opened PDF file, returning a
-     * minimal pdfdom.PDF structure (just trailer and cross_references)
-     */
-    PDFFileReader.prototype.readMetadata = function () {
-        // 1. find the final "startxref <offset> %%EOF", which should happen in the last 64 bytes
-        var footer = this.readRangeUntilString(this.stats.size - 64, '%%EOF');
-        if (footer == null)
-            throw new Error('Could not find final %%EOF');
-        var xref_offset = parseStartXref(footer.buffer);
-        // 2. seek to that xref and read until the trailer
-        var xref_content = this.readRangeUntilString(xref_offset, 'trailer');
-        var xref_string = xref_content.buffer.toString('ascii');
-        // term.print('xref_string: %j', xref_string);
-        var cross_references = xref_parser.parse(xref_string);
-        // 3. read the trailer
-        var trailer_content = this.readRangeUntilString(xref_content.end, 'startxref');
-        // skip over the "trailer" marker
-        var trailer_string = trailer_content.buffer.slice(7).toString('ascii').trim();
-        // term.print('trailer_string: %j', trailer_string);
-        var trailer_object = pdfobject_parser.parse(trailer_string);
-        // term.print('trailer_object: %j', trailer_object);
-        var trailer = trailer_object;
-        //var next_xref = indexed_file.readRangeUntil(trailer_content.end, '%%EOF');
-        // next_xref is just the XREF from the first lookup
-        return {
-            cross_references: cross_references,
-            trailer: trailer,
-        };
-    };
-    /** findObject()
-  
-    pdfdom.IndirectReference is just a simple interface:
-       {object_number: number, generation_number: number}
-    */
-    PDFFileReader.prototype.findObject = function (reference, cross_references) {
-        var cross_reference = findCrossReference(reference, cross_references);
-        // TODO: only match endobj at the beginning of lines
-        var object_content = this.readRangeUntilString(cross_reference.offset, 'endobj');
-        var object_string = object_content.buffer.toString('ascii');
-        var object = pdfobject_parser.parse(object_string);
-        // object is a pdfdom.IndirectObject, but we already knew the object number
-        // and generation number; that's how we found it. We only want the value of
-        // the object. But we might as well double check that what we got is what
-        // we were looking for:
-        if (object.object_number != cross_reference.object_number) {
-            throw new Error("PDF cross references are incorrect; the offset\n        " + cross_reference.offset + " does not lead to an object numbered\n        " + cross_reference.object_number + "; instead, the object at that offset is\n        " + object.object_number);
-        }
-        return object.value;
-    };
-    return PDFFileReader;
-})(FileCursor);
+    return object.value;
+}
 function open(filepath) {
-    var reader = new PDFFileReader(filepath);
-    var pdf = reader.readMetadata();
+    var file = File.open(filepath);
+    var reader = new FileReader(file);
+    var pdf = readMetadata(reader);
     term.print('cross_references', term.inspect(pdf.cross_references));
     term.print('trailer', pdf.trailer);
-    var Info = reader.findObject(pdf.trailer['Info'], pdf.cross_references);
+    var Info = findObject(file, pdf.trailer['Info'], pdf.cross_references);
     term.print('trailer->Info', Info);
-    var Root = reader.findObject(pdf.trailer['Root'], pdf.cross_references);
+    var Root = findObject(file, pdf.trailer['Root'], pdf.cross_references);
     term.print('trailer->Root', Root);
-    var Pages = reader.findObject(Root['Pages'], pdf.cross_references);
+    var Pages = findObject(file, Root['Pages'], pdf.cross_references);
     term.print('trailer->Root->Pages', Pages);
     var pages = Pages['Kids'];
     term.print('Found %d pages', pages.length);
     // iterate through the page objects
     var page_objects = [];
     for (var i = 0, page; (page = pages[i]); i++) {
-        var page_object = reader.findObject(page, pdf.cross_references);
+        var page_object = findObject(file, page, pdf.cross_references);
         page_objects.push(page_object);
         term.print('Page %d', i, page_object);
     }
@@ -150,7 +113,7 @@ function open(filepath) {
         // page_object.Contents is a list of IndirectReference instances, or maybe just one
         var page_contents = Array.isArray(page_object['Contents']) ? page_object['Contents'] : [page_object['Contents']];
         for (var j = 0, page_content; (page_content = page_contents[j]); j++) {
-            var content_object = reader.findObject(page_content, pdf.cross_references);
+            var content_object = findObject(file, page_content, pdf.cross_references);
             term.print('Page %d:%d', i, j, content_object);
         }
     }
--- structure.ts
+++ structure.ts
@@ -2,42 +2,17 @@
 import logger = require('loge');
 import chalk = require('chalk');
 
-import FileCursor = require('./FileCursor');
+import File = require('./File');
+import FileReader = require('./readers/FileReader');
+import BufferedFileReader = require('./readers/BufferedFileReader');
 
 import pdfdom = require('./pdfdom');
 
 import run = require('./dev/run');
 import term = require('./dev/term');
 
-function printParseException(input, exception) {
-  logger.error('(%d,%d): [%d/%d] %s', exception.line, exception.column,
-    exception.offset, input.length, exception.toString());
-  // 32 4 1 [ { type: 'class', value: '[0-9]', description: '[0-9]' },
-  var margin = 256; // input.length > 256 ? 64 : 256;
-  var prefix = input.slice(Math.max(0, exception.offset - margin), exception.offset);
-  var position = input.slice(exception.offset, exception.offset + 1)
-  var postfix = input.slice(exception.offset + 1, exception.offset + margin);
-  term.print(term.escape(prefix) + chalk.bgRed(term.escape(position)) + term.escape(postfix));
-  // exception.offset, exception.line, exception.column, exception.expected, exception.found);
-  // term.print(exc.offset, exc.line, exc.column, exc.expected, exc.found, exc.name, Object.keys(exc));
-}
-
-function wrapParser(parser) {
-  return {
-    parse: function(input) {
-      try {
-        return parser.parse(input);
-      }
-      catch (exc) {
-        printParseException(input, exc);
-        throw exc;
-      }
-    }
-  }
-}
-
-var pdfobject_parser = wrapParser(require('./parsers/pdfobject'));
-var xref_parser = wrapParser(require('./parsers/xref'));
+import pdfobject_parser = require('./parsers/pdfobject');
+import xref_parser = require('./parsers/xref');
 
 
 /** parseStartXref(buffer)
@@ -62,7 +37,6 @@ function parseStartXref(buffer: Buffer): number {
 Helper function for finding a CrossReference in a list of cross references,
 given an IndirectReference, throwing an error if no match is found.
 */
-
 function findCrossReference(reference: pdfdom.IndirectReference,
                             cross_references: pdfdom.CrossReference[]): pdfdom.CrossReference {
   for (var i = 0, cross_reference; (cross_reference = cross_references[i]); i++) {
@@ -77,84 +51,80 @@ function findCrossReference(reference: pdfdom.IndirectReference,
     ${reference.object_number}:${reference.generation_number}`);
 }
 
-class PDFFileReader extends FileCursor {
-  // the trailer will generally have two important fields: "Root" and "Info",
-  // both of which are object references
+/**
+Reads the final xref and trailer from the opened PDF file, returning a
+minimal pdfdom.PDF structure (just trailer and cross_references)
 
-  constructor(public filepath: string) {
-    super(filepath);
+The trailer will generally have two important fields: "Root" and "Info",
+both of which are object references.
+*/
+function readMetadata(reader: FileReader): pdfdom.PDF {
+  // 1. find the final "startxref <offset> %%EOF", which should happen in the last 64 bytes
+  var footer = reader.readRangeUntilString(reader.size - 64, '%%EOF');
+  if (footer == null) throw new Error('Could not find final %%EOF');
+  var xref_offset = parseStartXref(footer.buffer);
+
+  // 2. seek to that xref and read until the trailer
+  var xref_content = reader.readRangeUntilString(xref_offset, 'trailer');
+  var xref_string = xref_content.buffer.toString('ascii');
+  // term.print('xref_string: %j', xref_string);
+  var cross_references = xref_parser.parse(xref_string);
+
+  // 3. read the trailer
+  var trailer_content = reader.readRangeUntilString(xref_content.end, 'startxref');
+  // skip over the "trailer" marker
+  var trailer_string = trailer_content.buffer.slice(7).toString('ascii').trim();
+  // term.print('trailer_string: %j', trailer_string);
+  var trailer_object = pdfobject_parser.parseString(trailer_string);
+  // term.print('trailer_object: %j', trailer_object);
+  var trailer = <pdfdom.DictionaryObject>trailer_object;
+  //var next_xref = indexed_file.readRangeUntil(trailer_content.end, '%%EOF');
+  // next_xref is just the XREF from the first lookup
+  return {
+    cross_references: cross_references,
+    trailer: trailer,
+  };
+}
+
+/**
+Read a specific object from a PDF, given a desired reference and list of cross
+references.
+*/
+function findObject(file: File,
+                    reference: pdfdom.IndirectReference,
+                    cross_references: pdfdom.CrossReference[]): pdfdom.PDFObject {
+  var cross_reference = findCrossReference(reference, cross_references);
+  // TODO: only match endobj at the beginning of lines
+  // var object_content = reader.readRangeUntilString(cross_reference.offset, 'endobj');
+  // var object_string = object_content.buffer.toString('ascii');
+  var reader = new BufferedFileReader(file, cross_reference.offset);
+  var object = <pdfdom.IndirectObject>pdfobject_parser.parse(reader);
+  // object is a pdfdom.IndirectObject, but we already knew the object number
+  // and generation number; that's how we found it. We only want the value of
+  // the object. But we might as well double check that what we got is what
+  // we were looking for:
+  if (object.object_number != cross_reference.object_number) {
+    throw new Error(`PDF cross references are incorrect; the offset
+      ${cross_reference.offset} does not lead to an object numbered
+      ${cross_reference.object_number}; instead, the object at that offset is
+      ${object.object_number}`);
   }
-
-  /** PDFFileReader#readMetadata()
-   * Reads the final xref and trailer from the opened PDF file, returning a
-   * minimal pdfdom.PDF structure (just trailer and cross_references)
-   */
-  readMetadata(): pdfdom.PDF {
-    // 1. find the final "startxref <offset> %%EOF", which should happen in the last 64 bytes
-    var footer = this.readRangeUntilString(this.stats.size - 64, '%%EOF');
-    if (footer == null) throw new Error('Could not find final %%EOF');
-    var xref_offset = parseStartXref(footer.buffer);
-
-    // 2. seek to that xref and read until the trailer
-    var xref_content = this.readRangeUntilString(xref_offset, 'trailer');
-    var xref_string = xref_content.buffer.toString('ascii');
-    // term.print('xref_string: %j', xref_string);
-    var cross_references = xref_parser.parse(xref_string);
-
-    // 3. read the trailer
-    var trailer_content = this.readRangeUntilString(xref_content.end, 'startxref');
-    // skip over the "trailer" marker
-    var trailer_string = trailer_content.buffer.slice(7).toString('ascii').trim();
-    // term.print('trailer_string: %j', trailer_string);
-    var trailer_object = pdfobject_parser.parse(trailer_string);
-    // term.print('trailer_object: %j', trailer_object);
-    var trailer = <pdfdom.DictionaryObject>trailer_object;
-    //var next_xref = indexed_file.readRangeUntil(trailer_content.end, '%%EOF');
-    // next_xref is just the XREF from the first lookup
-    return {
-      cross_references: cross_references,
-      trailer: trailer,
-    };
-  }
-
-  /** findObject()
-
-  pdfdom.IndirectReference is just a simple interface:
-     {object_number: number, generation_number: number}
-  */
-  findObject(reference: pdfdom.IndirectReference,
-             cross_references: pdfdom.CrossReference[]): pdfdom.PDFObject {
-    var cross_reference = findCrossReference(reference, cross_references);
-    // TODO: only match endobj at the beginning of lines
-    var object_content = this.readRangeUntilString(cross_reference.offset, 'endobj');
-    var object_string = object_content.buffer.toString('ascii');
-    var object = pdfobject_parser.parse(object_string);
-    // object is a pdfdom.IndirectObject, but we already knew the object number
-    // and generation number; that's how we found it. We only want the value of
-    // the object. But we might as well double check that what we got is what
-    // we were looking for:
-    if (object.object_number != cross_reference.object_number) {
-      throw new Error(`PDF cross references are incorrect; the offset
-        ${cross_reference.offset} does not lead to an object numbered
-        ${cross_reference.object_number}; instead, the object at that offset is
-        ${object.object_number}`);
-    }
-    return object.value;
-  }
+  return object.value;
 }
 
 export function open(filepath: string): void {
-  var reader = new PDFFileReader(filepath);
+  var file = File.open(filepath);
+  var reader = new FileReader(file);
 
-  var pdf = reader.readMetadata();
+  var pdf = readMetadata(reader);
   term.print('cross_references', term.inspect(pdf.cross_references));
   term.print('trailer', pdf.trailer);
 
-  var Info = reader.findObject(<pdfdom.IndirectReference>pdf.trailer['Info'], pdf.cross_references);
+  var Info = findObject(file, <pdfdom.IndirectReference>pdf.trailer['Info'], pdf.cross_references);
   term.print('trailer->Info', Info);
-  var Root = reader.findObject(<pdfdom.IndirectReference>pdf.trailer['Root'], pdf.cross_references);
+  var Root = findObject(file, <pdfdom.IndirectReference>pdf.trailer['Root'], pdf.cross_references);
   term.print('trailer->Root', Root);
-  var Pages = reader.findObject(<pdfdom.IndirectReference>Root['Pages'], pdf.cross_references);
+  var Pages = findObject(file, <pdfdom.IndirectReference>Root['Pages'], pdf.cross_references);
   term.print('trailer->Root->Pages', Pages);
 
   var pages = <pdfdom.ArrayObject>Pages['Kids'];
@@ -163,7 +133,7 @@ export function open(filepath: string): void {
   // iterate through the page objects
   var page_objects: pdfdom.PDFObject[] = [];
   for (var i = 0, page; (page = pages[i]); i++) {
-    var page_object = reader.findObject(<pdfdom.IndirectReference>page, pdf.cross_references);
+    var page_object = findObject(file, <pdfdom.IndirectReference>page, pdf.cross_references);
     page_objects.push(page_object);
     term.print('Page %d', i, page_object);
   }
@@ -172,7 +142,7 @@ export function open(filepath: string): void {
     // page_object.Contents is a list of IndirectReference instances, or maybe just one
     var page_contents = Array.isArray(page_object['Contents']) ? page_object['Contents'] : [page_object['Contents']];
     for (var j = 0, page_content; (page_content = page_contents[j]); j++) {
-      var content_object = reader.findObject(<pdfdom.IndirectReference>page_content, pdf.cross_references);
+      var content_object = findObject(file, <pdfdom.IndirectReference>page_content, pdf.cross_references);
       term.print('Page %d:%d', i, j, content_object);
     }
   }
