0070558f67c5f8909be810708170f928468e1b34
Christopher Brown <io@henrian.com>
2015-02-25 16:10:35 -0600

    Simplify FileReader; keep track of position in file

--- readers/FileReader.js
+++ readers/FileReader.js
@@ -2,8 +2,10 @@ var bufferops = require('../bufferops');
 /** A representation of an open file with some functions to aid reading.
  */
 var FileReader = (function () {
-    function FileReader(file) {
+    function FileReader(file, position) {
+        if (position === void 0) { position = 0; }
         this.file = file;
+        this.position = position;
     }
     Object.defineProperty(FileReader.prototype, "size", {
         get: function () {
@@ -13,72 +15,59 @@ var FileReader = (function () {
         configurable: true
     });
     /**
-    Read the next `length` bytes of the underlying file as a Buffer.
+    Read the next `length` bytes of the underlying file as a Buffer. May return a
+    Buffer shorter than `length` iff EOF has been reached.
     */
     FileReader.prototype.readBuffer = function (length) {
         var buffer = new Buffer(length);
-        var bytesRead = this.file.read(buffer, 0, length, null);
+        var bytesRead = this.file.read(buffer, 0, length, this.position);
+        this.position += bytesRead;
         if (bytesRead < length) {
             buffer = buffer.slice(0, bytesRead);
         }
         return buffer;
     };
+    // /**
+    // Calls readRangeUntilBuffer(needle: Buffer) after converting the
+    // given string to a Buffer.
+    // */
+    // readUntilString(needle: string): ByteRange {
+    //   return this.readUntilBuffer(new Buffer(needle));
+    // }
     /**
-    Read the next block of the underlying file as a Buffer.
+    Starting at the current position, read until EOF or we find `needle`,
+    whichever happens first.
+  
+    1. If we do find needle, return the position of it within the file, and set
+       the current position to point at the first character of `needle`.
+    2. If we can't find it, return null.
     */
-    FileReader.prototype.readBlock = function () {
-        return this.readBuffer(FileReader.BLOCK_SIZE);
-    };
-    /**
-    Calls readRangeUntilBuffer(start, needle: Buffer) after converting the
-    given string to a Buffer
-    */
-    FileReader.prototype.readRangeUntilString = function (start, needle) {
-        return this.readRangeUntilBuffer(start, new Buffer(needle));
-    };
-    /**
-     * Starting at 'start', read until EOF or we find `needle`,
-     * whichever happens first.
-     *
-     * 1. If we do find needle, return the intervening content as a Buffer, and
-     *    advance the offset to point to the first character beyond `needle`.
-     * 2. If we can't find it, return null.
-     *
-     * I'm not totally happy with the way TypeScript does overloading. I mean,
-     * it's true to the Javascript, but they could add a little sugar, I think,
-     * to avoid typeof, etc., comparisons, no? They need some type assertion
-     * sugar, too.
-     */
-    FileReader.prototype.readRangeUntilBuffer = function (start, needle) {
-        var position = start;
+    FileReader.prototype.indexOf = function (needle_string) {
+        var needle = new Buffer(needle_string);
         var haystack = new Buffer(0);
+        var haystack_file_position = this.position;
         var haystack_search_offset = 0;
         var block_buffer = new Buffer(FileReader.BLOCK_SIZE);
         var bytesRead = FileReader.BLOCK_SIZE;
         while (bytesRead == FileReader.BLOCK_SIZE) {
             // we use the position once, to seek, and then set it to null, to use the
             // current position on subsequent reads. Hopefully no one else has seeked
-            // on this file descriptor by the next time we use it!
-            // TODO: figure out why setting it to null wraps around to the beginning
+            // on this file descriptor by the next time we use it! But wait...
+            // TODO: figure out why setting position it to null wraps around to the beginning
             //       see s/position += bytesRead;/position = null/ below
-            bytesRead = this.file.read(block_buffer, 0, FileReader.BLOCK_SIZE, position);
-            //logger.debug('[FileReader] read %d bytes', bytesRead);
-            //logger.debug('bytes: %s', block_buffer.toString('ascii', 0, bytesRead));
-            position += bytesRead;
+            bytesRead = this.file.read(block_buffer, 0, FileReader.BLOCK_SIZE, this.position);
+            this.position += bytesRead;
             // append new block to stateful buffer; block_buffer may have extra bytes
             // at the end, so only read up to the readBytes mark
             haystack = Buffer.concat([haystack, block_buffer], haystack.length + bytesRead);
             // TODO: only start looking in the haystack (`buffer`) at the old
             //   offset, backtracking by needle.length in case there's a partial match
-            var haystack_needle_index = bufferops.indexOf(haystack, needle, haystack_search_offset);
-            // haystack_needle_index, if not null, is the length of the result range
-            if (haystack_needle_index !== null) {
+            var needle_haystack_index = bufferops.indexOf(haystack, needle, haystack_search_offset);
+            // needle_haystack_index, if not null, is the position of needle within haystack
+            if (needle_haystack_index !== null) {
                 // we found it!
-                return {
-                    buffer: haystack.slice(0, haystack_needle_index),
-                    start: start,
-                    end: start + haystack_needle_index,
-                };
+                this.position = haystack_file_position + needle_haystack_index;
+                return haystack_file_position + needle_haystack_index;
             }
         }
         // logger.debug(`FileReader#readRangeUntilBuffer: failed to find ${needle} in ${haystack}`);
--- readers/FileReader.ts
+++ readers/FileReader.ts
@@ -3,39 +3,25 @@ import fs = require('fs');
 import File = require('../File');
 import bufferops = require('../bufferops');
 
-/** ByteRange: used to designate a portion of a file.
- * This allows us to specify where to start
- *
- * buffer: the bytes of the file segment
- * start: the byte offset within the original file of the first byte in buffer
- * end: the byte offset within the original file of the character directly
- *      following the last byte in buffer.
- *
- * buffer.length should equal (end - start)
- */
-interface ByteRange {
-  buffer: Buffer;
-  start: number;
-  end: number;
-}
-
 /** A representation of an open file with some functions to aid reading.
  */
 class FileReader {
   static BLOCK_SIZE = 1024;
 
-  constructor(private file: File) { }
+  constructor(private file: File, private position: number = 0) { }
 
   get size(): number {
     return this.file.size;
   }
 
   /**
-  Read the next `length` bytes of the underlying file as a Buffer.
+  Read the next `length` bytes of the underlying file as a Buffer. May return a
+  Buffer shorter than `length` iff EOF has been reached.
   */
   readBuffer(length: number): Buffer {
     var buffer = new Buffer(length);
-    var bytesRead = this.file.read(buffer, 0, length, null);
+    var bytesRead = this.file.read(buffer, 0, length, this.position);
+    this.position += bytesRead;
     if (bytesRead < length) {
       buffer = buffer.slice(0, bytesRead);
     }
@@ -43,36 +29,19 @@ class FileReader {
   }
 
   /**
-  Read the next block of the underlying file as a Buffer.
-  */
-  readBlock(): Buffer {
-    return this.readBuffer(FileReader.BLOCK_SIZE);
-  }
+  Starting at the current position, read until EOF or we find `needle`,
+  whichever happens first.
 
-  /**
-  Calls readRangeUntilBuffer(start, needle: Buffer) after converting the
-  given string to a Buffer
-  */
-  readRangeUntilString(start: number, needle: string): ByteRange {
-    return this.readRangeUntilBuffer(start, new Buffer(needle));
-  }
+  1. If we do find needle, return the position of it within the file, and set
+     the current position to point at the first character of `needle`.
+  2. If we can't find it, return null.
 
-  /**
-   * Starting at 'start', read until EOF or we find `needle`,
-   * whichever happens first.
-   *
-   * 1. If we do find needle, return the intervening content as a Buffer, and
-   *    advance the offset to point to the first character beyond `needle`.
-   * 2. If we can't find it, return null.
-   *
-   * I'm not totally happy with the way TypeScript does overloading. I mean,
-   * it's true to the Javascript, but they could add a little sugar, I think,
-   * to avoid typeof, etc., comparisons, no? They need some type assertion
-   * sugar, too.
-   */
-  readRangeUntilBuffer(start: number, needle: Buffer): ByteRange {
-    var position = start;
+  TODO: add optional fromIndex argument.
+  */
+  indexOf(searchValue: string): number {
+    var needle = new Buffer(searchValue);
     var haystack = new Buffer(0);
+    var haystack_file_position = this.position;
     var haystack_search_offset = 0;
     var block_buffer = new Buffer(FileReader.BLOCK_SIZE);
     var bytesRead = FileReader.BLOCK_SIZE;
@@ -80,30 +49,24 @@ class FileReader {
     while (bytesRead == FileReader.BLOCK_SIZE) {
       // we use the position once, to seek, and then set it to null, to use the
       // current position on subsequent reads. Hopefully no one else has seeked
-      // on this file descriptor by the next time we use it!
-      // TODO: figure out why setting it to null wraps around to the beginning
+      // on this file descriptor by the next time we use it! But wait...
+      // TODO: figure out why setting position it to null wraps around to the beginning
       //       see s/position += bytesRead;/position = null/ below
-      bytesRead = this.file.read(block_buffer, 0, FileReader.BLOCK_SIZE, position);
-      //logger.debug('[FileReader] read %d bytes', bytesRead);
-      //logger.debug('bytes: %s', block_buffer.toString('ascii', 0, bytesRead));
-      position += bytesRead;
+      bytesRead = this.file.read(block_buffer, 0, FileReader.BLOCK_SIZE, this.position);
+      this.position += bytesRead;
       // append new block to stateful buffer; block_buffer may have extra bytes
       // at the end, so only read up to the readBytes mark
       haystack = Buffer.concat([haystack, block_buffer], haystack.length + bytesRead);
       // TODO: only start looking in the haystack (`buffer`) at the old
       //   offset, backtracking by needle.length in case there's a partial match
-      var haystack_needle_index = bufferops.indexOf(haystack, needle, haystack_search_offset);
-      // haystack_needle_index, if not null, is the length of the result range
-      if (haystack_needle_index !== null) {
+      var needle_haystack_index = bufferops.indexOf(haystack, needle, haystack_search_offset);
+      // needle_haystack_index, if not null, is the position of needle within haystack
+      if (needle_haystack_index !== null) {
         // we found it!
-        return {
-          buffer: haystack.slice(0, haystack_needle_index),
-          start: start,
-          end: start + haystack_needle_index,
-        };
+        this.position = haystack_file_position + needle_haystack_index;
+        return haystack_file_position + needle_haystack_index;
       }
     }
-    // logger.debug(`FileReader#readRangeUntilBuffer: failed to find ${needle} in ${haystack}`);
     // we hit EOF before finding needle; return null
     return null;
   }
