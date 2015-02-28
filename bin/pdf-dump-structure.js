13fa1f96d0c339a695b3aa39b2f1a640da8e1536
Christopher Brown <io@henrian.com>
2015-02-28 16:05:57 -0600

    Move structure.open pdf metadata/page dump into pdf-dump-structure bin script

--- bin/pdf-dump-structure.js
+++ bin/pdf-dump-structure.js
@@ -1,13 +1,10 @@
 #!/usr/bin/env node
-var fs = require('fs');
 var logger = require('loge');
-var run = require('../dev/run');
-var structure = require('../structure');
+var term = require('../dev/term');
+var PDF = require('../PDF');
 
 var yargs = require('yargs')
-  .usage([
-    'Usage: $0 -f ',
-  ].join('\n'))
+  .usage('Usage: $0 -f ScienceArticle.pdf')
   .describe({
     filename: 'pdf file to open',
     help: 'print this help message',
@@ -26,7 +23,29 @@ var yargs = require('yargs')
 var argv = yargs.argv;
 logger.level = argv.verbose ? 'debug' : 'info';
 
-if (require.main == module) run(function(callback) {
-  structure.open(argv.filename);
-  callback();
-});
+function main() {
+  var pdf = PDF.open(argv.filename);
+  term.print('trailer', pdf.trailer);
+  term.print('cross_references', term.inspect(pdf.cross_references));
+  var Info = pdf.resolveObject(pdf.trailer.Info);
+  term.print('trailer->Info', Info);
+  var Root = pdf.resolveObject(pdf.trailer.Root);
+  term.print('trailer->Root', Root);
+  var Pages = pdf.resolveObject(Root.Pages);
+  term.print('trailer->Root->Pages', Pages);
+
+  var pages = pdf.pages;
+  term.print('Found %d pages', pages.length);
+
+  // iterate through the page objects
+  for (var i = 0, page; (page = pages[i]); i++) {
+    // page_object.Contents is a list of IndirectReference instances, or maybe just one
+    var contents = Array.isArray(page.Contents) ? page.Contents : [page.Contents];
+    for (var j = 0, content; (content = contents[j]); j++) {
+        var content_object = pdf.resolveObject(content);
+        term.print('Page %d:%d', i, j, content_object);
+    }
+  }
+}
+
+if (require.main == module) main();
