efb5d2bf24bf8adb07e3f42d9046fe6f89fbac12
Christopher Brown <io@henrian.com>
2015-03-05 11:15:14 -0600

    Add lastIndexOf to FileReader and remove file position state from FileReader instances

--- readers/FileReader.js
+++ readers/FileReader.js
@@ -2,10 +2,8 @@ var bufferops = require('../bufferops');
 /** A representation of an open file with some functions to aid reading.
  */
 var FileReader = (function () {
-    function FileReader(file, position) {
-        if (position === void 0) { position = 0; }
+    function FileReader(file) {
         this.file = file;
-        this.position = position;
     }
     Object.defineProperty(FileReader.prototype, "size", {
         get: function () {
@@ -15,29 +13,17 @@ var FileReader = (function () {
         configurable: true
     });
     /**
-    Read the next `length` bytes of the underlying file as a Buffer. May return a
-    Buffer shorter than `length` iff EOF has been reached.
-    */
-    FileReader.prototype.readBuffer = function (length) {
-        var buffer = this.file.readBuffer(length, this.position);
-        // `buffer.length` might not equal `length`!
-        this.position += buffer.length;
-        return buffer;
-    };
-    /**
-    Starting at the current position, read until EOF or we find `needle`,
-    whichever happens first.
+    Starting at fromIndex (defaults to 0), read until we find `needle`.
   
-    1. If we do find needle, return the position of it within the file, and set
-       the current position to point at the first character of `needle`.
-    2. If we can't find it, return null.
-  
-    TODO: add optional fromIndex argument.
+    1. If we do find needle, return the file's position of the first character of `needle`.
+    2. If we reach EOF without finding it, return null.
     */
-    FileReader.prototype.indexOf = function (searchValue) {
+    FileReader.prototype.indexOf = function (searchValue, fromIndex) {
+        if (fromIndex === void 0) { fromIndex = 0; }
         var needle = new Buffer(searchValue);
+        var position = fromIndex;
         var haystack = new Buffer(0);
-        var haystack_file_position = this.position;
+        var haystack_file_position = position;
         var haystack_search_offset = 0;
         var block_buffer = new Buffer(FileReader.BLOCK_SIZE);
         var bytesRead = FileReader.BLOCK_SIZE;
@@ -47,8 +33,8 @@ var FileReader = (function () {
             // on this file descriptor by the next time we use it! But wait...
             // TODO: figure out why setting position it to null wraps around to the beginning
             //       see s/position += bytesRead;/position = null/ below
-            bytesRead = this.file.read(block_buffer, 0, FileReader.BLOCK_SIZE, this.position);
-            this.position += bytesRead;
+            bytesRead = this.file.read(block_buffer, 0, FileReader.BLOCK_SIZE, position);
+            position += bytesRead;
             // append new block to stateful buffer; block_buffer may have extra bytes
             // at the end, so only read up to the readBytes mark
             haystack = Buffer.concat([haystack, block_buffer], haystack.length + bytesRead);
@@ -58,8 +44,33 @@ var FileReader = (function () {
             // needle_haystack_index, if not null, is the position of needle within haystack
             if (needle_haystack_index !== null) {
                 // we found it!
-                this.position = haystack_file_position + needle_haystack_index;
                 return haystack_file_position + needle_haystack_index;
+            }
+        }
+        // we hit EOF before finding needle; return null
+        return null;
+    };
+    /**
+    Starting at fromIndex (defaults to EOF), read backwards until we find `needle`.
+  
+    1. If we do find needle, return the file's position of the first character of `needle`.
+    2. If we reach the beginning of the file before finding it, return null.
+    */
+    FileReader.prototype.lastIndexOf = function (searchValue, fromIndex) {
+        if (fromIndex === void 0) { fromIndex = this.file.size; }
+        var needle = new Buffer(searchValue);
+        var position = fromIndex;
+        var haystack = new Buffer(0);
+        while (position > -1) {
+            position -= FileReader.BLOCK_SIZE;
+            var block_buffer = this.file.readBuffer(FileReader.BLOCK_SIZE, position);
+            haystack = Buffer.concat([block_buffer, haystack]);
+            // TODO: only start looking in the haystack (`buffer`) at the old
+            //   offset, backtracking by needle.length in case there's a partial match
+            var last_index_of_needle_within_haystack = bufferops.lastIndexOf(haystack, needle);
+            if (last_index_of_needle_within_haystack !== null) {
+                // we found it!
+                return position + last_index_of_needle_within_haystack;
             }
         }
         // we hit EOF before finding needle; return null
--- readers/FileReader.ts
+++ readers/FileReader.ts
@@ -8,37 +8,23 @@ import bufferops = require('../bufferops');
 class FileReader {
   static BLOCK_SIZE = 1024;
 
-  constructor(private file: File, private position: number = 0) { }
+  constructor(private file: File) { }
 
   get size(): number {
     return this.file.size;
   }
 
   /**
-  Read the next `length` bytes of the underlying file as a Buffer. May return a
-  Buffer shorter than `length` iff EOF has been reached.
+  Starting at fromIndex (defaults to 0), read until we find `needle`.
+
+  1. If we do find needle, return the file's position of the first character of `needle`.
+  2. If we reach EOF without finding it, return null.
   */
-  readBuffer(length: number): Buffer {
-    var buffer = this.file.readBuffer(length, this.position);
-    // `buffer.length` might not equal `length`!
-    this.position += buffer.length;
-    return buffer;
-  }
-
-  /**
-  Starting at the current position, read until EOF or we find `needle`,
-  whichever happens first.
-
-  1. If we do find needle, return the position of it within the file, and set
-     the current position to point at the first character of `needle`.
-  2. If we can't find it, return null.
-
-  TODO: add optional fromIndex argument.
-  */
-  indexOf(searchValue: string): number {
+  indexOf(searchValue: string, fromIndex: number = 0): number {
     var needle = new Buffer(searchValue);
+    var position = fromIndex;
     var haystack = new Buffer(0);
-    var haystack_file_position = this.position;
+    var haystack_file_position = position;
     var haystack_search_offset = 0;
     var block_buffer = new Buffer(FileReader.BLOCK_SIZE);
     var bytesRead = FileReader.BLOCK_SIZE;
@@ -49,8 +35,8 @@ class FileReader {
       // on this file descriptor by the next time we use it! But wait...
       // TODO: figure out why setting position it to null wraps around to the beginning
       //       see s/position += bytesRead;/position = null/ below
-      bytesRead = this.file.read(block_buffer, 0, FileReader.BLOCK_SIZE, this.position);
-      this.position += bytesRead;
+      bytesRead = this.file.read(block_buffer, 0, FileReader.BLOCK_SIZE, position);
+      position += bytesRead;
       // append new block to stateful buffer; block_buffer may have extra bytes
       // at the end, so only read up to the readBytes mark
       haystack = Buffer.concat([haystack, block_buffer], haystack.length + bytesRead);
@@ -60,8 +46,35 @@ class FileReader {
       // needle_haystack_index, if not null, is the position of needle within haystack
       if (needle_haystack_index !== null) {
         // we found it!
-        this.position = haystack_file_position + needle_haystack_index;
         return haystack_file_position + needle_haystack_index;
+      }
+    }
+    // we hit EOF before finding needle; return null
+    return null;
+  }
+
+  /**
+  Starting at fromIndex (defaults to EOF), read backwards until we find `needle`.
+
+  1. If we do find needle, return the file's position of the first character of `needle`.
+  2. If we reach the beginning of the file before finding it, return null.
+  */
+  lastIndexOf(searchValue: string, fromIndex: number = this.file.size): number {
+    var needle = new Buffer(searchValue);
+    var position = fromIndex;
+    var haystack = new Buffer(0);
+    // haystack's position within file is always equal to `position`
+    // exit loop once we reach the beginning of the file.
+    while (position > -1) {
+      position -= FileReader.BLOCK_SIZE;
+      var block_buffer = this.file.readBuffer(FileReader.BLOCK_SIZE, position);
+      haystack = Buffer.concat([block_buffer, haystack]);
+      // TODO: only start looking in the haystack (`buffer`) at the old
+      //   offset, backtracking by needle.length in case there's a partial match
+      var last_index_of_needle_within_haystack = bufferops.lastIndexOf(haystack, needle);
+      if (last_index_of_needle_within_haystack !== null) {
+        // we found it!
+        return position + last_index_of_needle_within_haystack;
       }
     }
     // we hit EOF before finding needle; return null
