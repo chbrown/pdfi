e4e64ebb29486afc602fbf01bbfe593842bd128d
Christopher Brown <io@henrian.com>
2015-03-22 18:00:37 -0500

    Do less parsing in the CMap parser lexer, and more in the (new) combiner

--- parsers/cmap.js
+++ parsers/cmap.js
@@ -4,100 +4,135 @@ var Token = lexing.Token;
 function parseHex(hexadecimal) {
     return parseInt(hexadecimal, 16);
 }
+function mkString(charCodes) {
+    return String.fromCharCode.apply(null, charCodes);
+}
+function parseHexstring(hexstring, charWidth) {
+    var charCodes = [];
+    for (var i = 0; i < hexstring.length; i += charWidth) {
+        var charHexadecimal = hexstring.slice(i, i + charWidth);
+        charCodes.push(parseHex(charHexadecimal));
+    }
+    return mkString(charCodes);
+}
 var default_rules = [
     [/^$/, function (match) { return Token('EOF'); }],
     [/^\s+/, function (match) { return null; }],
-    [/^beginbfrange/, function (match) {
-        this.states.push('BFRANGE');
-        // return Token('START', 'BFRANGE');
-        return null;
-    }],
-    [/^begincodespacerange/, function (match) {
-        this.states.push('CODESPACERANGE');
-        // return Token('START', 'CODESPACERANGE');
-        return null;
-    }],
-    [/^beginbfchar/, function (match) {
-        this.states.push('BFCHAR');
-        // return Token('START', 'BFCHAR');
-        return null;
-    }],
-    [/^\/([-A-Za-z0-9]+)/, function (match) { return Token('NAME', match[1]); }],
-    [/^\w+/, function (match) { return Token('STRING', match[0]); }],
-    [/^-?\d+/, function (match) { return Token('NUMBER', parseInt(match[0], 10)); }],
+    [/^beginbfrange/, function (match) { return Token('START', 'BFRANGE'); }],
+    [/^endbfrange/, function (match) { return Token('END', 'BFRANGE'); }],
+    [/^begincodespacerange/, function (match) { return Token('START', 'CODESPACERANGE'); }],
+    [/^endcodespacerange/, function (match) { return Token('END', 'CODESPACERANGE'); }],
+    [/^beginbfchar/, function (match) { return Token('START', 'BFCHAR'); }],
+    [/^endbfchar/, function (match) { return Token('END', 'BFCHAR'); }],
     [/^<</, function (match) { return Token('START', 'DICTIONARY'); }],
     [/^>>/, function (match) { return Token('END', 'DICTIONARY'); }],
     [/^\(/, function (match) { return Token('START', 'STRING'); }],
     [/^\)/, function (match) { return Token('END', 'STRING'); }],
+    [/^\[/, function (match) { return Token('START', 'ARRAY'); }],
+    [/^\]/, function (match) { return Token('END', 'ARRAY'); }],
+    [/^<([A-Fa-f0-9 ]+)>/, function (match) {
+        return Token('HEXSTRING', match[1].replace(/ /g, ''));
+    }],
+    [/^\/([!-'*-.0-;=?-Z\\^-z|~]+)/, function (match) { return Token('NAME', match[1]); }],
+    [/^\w+/, function (match) { return Token('COMMAND', match[0]); }],
+    [/^-?\d+/, function (match) { return Token('NUMBER', parseInt(match[0], 10)); }],
+    [/^(.|\n|\r)/, function (match) { return Token('CHAR', match[0]); }],
 ];
-var state_rules = {};
-state_rules['CODESPACERANGE'] = [
-    [/^endcodespacerange/, function (match) {
-        this.states.pop();
-        // return Token('END', 'CODESPACERANGE');
-        return null;
+var combiner_rules = [
+    ['ARRAY', function (tokens) {
+        return Token('ARRAY', tokens.map(function (token) { return token.value; }));
     }],
-    [/^\s+/, function (match) { return null; }],
-    [/^<(\w+)>\s*<(\w+)>/, function (match) {
-        return Token('CODESPACERANGE', {
-            start: parseHex(match[1]),
-            end: parseHex(match[2]),
-        });
+    ['STRING', function (tokens) {
+        return Token('STRING', tokens.map(function (token) { return token.value; }).join(''));
     }],
-];
-state_rules['BFRANGE'] = [
-    [/^endbfrange/, function (match) {
-        this.states.pop();
-        // return Token('END', 'BFRANGE');
-        return null;
+    ['DICTIONARY', function (tokens) {
+        return Token('DICTIONARY', tokens.map(function (token) { return token.value; }).join(''));
     }],
-    [/^\s+/, function (match) { return null; }],
-    [/^<(\w+)>\s*<(\w+)>\s*<(\w+)>/, function (match) {
-        return Token('BFRANGE', {
-            start: parseHex(match[1]),
-            end: parseHex(match[2]),
-            offset: parseHex(match[3]),
-        });
+    ['CODESPACERANGE', function (tokens) {
+        // tokens: [HEX, HEX]+
+        var values = tokens.map(function (token) { return token.value; });
+        var codespaceranges = [];
+        for (var i = 0; i < values.length; i += 2) {
+            codespaceranges.push(values.slice(i, i + 2));
+        }
+        return Token('CODESPACERANGES', codespaceranges);
     }],
-];
-state_rules['BFCHAR'] = [
-    [/^endbfchar/, function (match) {
-        this.states.pop();
-        // return Token('END', 'CODESPACERANGE');
-        return null;
+    ['BFRANGE', function (tokens) {
+        // tokens: [HEX, HEX, HEX | ARRAY<HEX>]+
+        var values = tokens.map(function (token) { return token.value; });
+        var bfranges = [];
+        for (var i = 0; i < values.length; i += 3) {
+            bfranges.push(values.slice(i, i + 3));
+        }
+        return Token('BFRANGES', bfranges);
     }],
-    [/^\s+/, function (match) { return null; }],
-    [/^<([A-Za-z0-9]+)>\s*<([A-Za-z0-9 ]+)>/, function (match) {
-        // String.fromCharCode(parseInt('D840', 16), parseInt('DC3E', 16))
-        return Token('BFCHAR', {
-            from: parseInt(match[1], 16),
-            to: match[2].replace(/\s+/, ''),
-        });
+    ['BFCHAR', function (tokens) {
+        // tokens: [HEX, HEX]+
+        var values = tokens.map(function (token) { return token.value; });
+        var bfchars = [];
+        for (var i = 0; i < values.length; i += 2) {
+            bfchars.push(values.slice(i, i + 2));
+        }
+        return Token('BFCHARS', bfchars);
     }],
 ];
 var CMapParser = (function () {
     function CMapParser() {
-        this.tokenizer = new lexing.Tokenizer(default_rules, state_rules);
+        this.tokenizer = new lexing.Tokenizer(default_rules);
+        this.combiner = new lexing.Combiner(combiner_rules);
     }
     /**
     Returns a mapping from in-PDF character codes to native Javascript Unicode strings
     */
     CMapParser.prototype.parse = function (iterable) {
         var token_iterator = this.tokenizer.map(iterable);
+        var combined_iterator = this.combiner.map(token_iterator);
         var mapping = [];
+        var applyBFRangeOffset = function (start, end, offset) {
+            for (var i = 0; (start + i) <= end; i++) {
+                mapping[start + i] = mkString([offset + i]);
+            }
+        };
+        var applyBFRangeArray = function (start, end, array) {
+            for (var i = 0; (start + i) <= end; i++) {
+                mapping[start + i] = array[i];
+            }
+        };
+        var applyBFChar = function (from, to) {
+            mapping[from] = to;
+        };
         while (1) {
-            var token = token_iterator.next();
+            var token = combined_iterator.next();
             if (token.name === 'EOF') {
                 break;
             }
-            else if (token.name === 'BFRANGE') {
-                for (var i = 0; (token.value.start + i) <= token.value.end; i++) {
-                    mapping[token.value.start + i] = String.fromCharCode(token.value.offset + i);
-                }
+            else if (token.name === 'CODESPACERANGES') {
+                token.value.forEach(function (tuple) {
+                    var start = parseHex(tuple[0]);
+                    var end = parseHex(tuple[1]);
+                });
             }
-            else if (token.name === 'BFCHAR') {
-                var charCodes = token.value.to.match(/.{4}/g).map(parseHex);
-                mapping[token.value.from] = String.fromCharCode.apply(null, charCodes);
+            else if (token.name === 'BFRANGES') {
+                token.value.forEach(function (triple) {
+                    var start = parseHex(triple[0]);
+                    var end = parseHex(triple[1]);
+                    var arg = triple[2];
+                    if (Array.isArray(arg)) {
+                        var array = arg.map(function (item) { return parseHexstring(item, 4); });
+                        applyBFRangeArray(start, end, array);
+                    }
+                    else {
+                        var offset = parseHex(arg);
+                        applyBFRangeOffset(start, end, offset);
+                    }
+                });
+            }
+            else if (token.name === 'BFCHARS') {
+                token.value.forEach(function (tuple) {
+                    var from = parseHex(tuple[0]);
+                    var to = parseHexstring(tuple[1], 4);
+                    applyBFChar(from, to);
+                });
             }
         }
         return mapping;
--- parsers/cmap.ts
+++ parsers/cmap.ts
@@ -7,112 +7,152 @@ function parseHex(hexadecimal: string): number {
   return parseInt(hexadecimal, 16);
 }
 
+function mkString(charCodes: number[]): string {
+  return String.fromCharCode.apply(null, charCodes);
+}
+
+function parseHexstring(hexstring: string, charWidth: number): string {
+  var charCodes: number[] = [];
+  for (var i = 0; i < hexstring.length; i += charWidth) {
+    var charHexadecimal = hexstring.slice(i, i + charWidth);
+    charCodes.push(parseHex(charHexadecimal));
+  }
+  return mkString(charCodes);
+}
+
 var default_rules: lexing.RegexRule<any>[] = [
   [/^$/, match => Token('EOF') ],
   [/^\s+/, match => null ], // skip over whitespace
-  [/^beginbfrange/, function(match) {
-    this.states.push('BFRANGE');
-    // return Token('START', 'BFRANGE');
-    return null;
-  }],
-  [/^begincodespacerange/, function(match) {
-    this.states.push('CODESPACERANGE');
-    // return Token('START', 'CODESPACERANGE');
-    return null;
-  }],
-  [/^beginbfchar/, function(match) {
-    this.states.push('BFCHAR');
-    // return Token('START', 'BFCHAR');
-    return null;
-  }],
-  [/^\/([-A-Za-z0-9]+)/, match => Token('NAME', match[1]) ],
-  [/^\w+/, match => Token('STRING', match[0]) ],
-  [/^-?\d+/, match => Token('NUMBER', parseInt(match[0], 10)) ],
+  // sections delimiters:
+  [/^beginbfrange/, match => Token('START', 'BFRANGE') ],
+  [/^endbfrange/, match => Token('END', 'BFRANGE') ],
+  [/^begincodespacerange/, match => Token('START', 'CODESPACERANGE') ],
+  [/^endcodespacerange/, match => Token('END', 'CODESPACERANGE') ],
+  [/^beginbfchar/, match => Token('START', 'BFCHAR') ],
+  [/^endbfchar/, match => Token('END', 'BFCHAR') ],
+  // other complex types
   [/^<</, match => Token('START', 'DICTIONARY') ],
   [/^>>/, match => Token('END', 'DICTIONARY') ],
   [/^\(/, match => Token('START', 'STRING') ],
   [/^\)/, match => Token('END', 'STRING') ],
+  [/^\[/, match => Token('START', 'ARRAY') ],
+  [/^\]/, match => Token('END', 'ARRAY') ],
+  // atomic types
+  [/^<([A-Fa-f0-9 ]+)>/, match => {
+    return Token('HEXSTRING', match[1].replace(/ /g, ''));
+  }],
+  // the rest of this we pretty much ignore
+  [/^\/([!-'*-.0-;=?-Z\\^-z|~]+)/, match => Token('NAME', match[1]) ],
+  [/^\w+/, match => Token('COMMAND', match[0]) ],
+  [/^-?\d+/, match => Token('NUMBER', parseInt(match[0], 10)) ],
+  [/^(.|\n|\r)/, match => Token('CHAR', match[0]) ],
 ];
 
-var state_rules: {[index: string]: lexing.RegexRule<any>[]} = {};
-state_rules['CODESPACERANGE'] = [
-  [/^endcodespacerange/, function(match) {
-    this.states.pop();
-    // return Token('END', 'CODESPACERANGE');
-    return null;
+var combiner_rules: lexing.CombinerRule<any, any>[] = [
+  ['ARRAY', tokens => {
+    return Token('ARRAY', tokens.map(token => token.value));
   }],
-  [/^\s+/, match => null ], // skip over whitespace
-  [/^<(\w+)>\s*<(\w+)>/, match => {
-    return Token('CODESPACERANGE', {
-      start: parseHex(match[1]),
-      end: parseHex(match[2]),
-    });
+  ['STRING', tokens => {
+    return Token('STRING', tokens.map(token => token.value).join(''));
   }],
-];
-state_rules['BFRANGE'] = [
-  [/^endbfrange/, function(match) {
-    this.states.pop();
-    // return Token('END', 'BFRANGE');
-    return null;
+  ['DICTIONARY', tokens => {
+    return Token('DICTIONARY', tokens.map(token => token.value).join(''));
   }],
-  [/^\s+/, match => null ], // skip over whitespace
-  [/^<(\w+)>\s*<(\w+)>\s*<(\w+)>/, match => {
-    return Token('BFRANGE', {
-      start: parseHex(match[1]),
-      end: parseHex(match[2]),
-      offset: parseHex(match[3]),
-    });
+  ['CODESPACERANGE', tokens => {
+    // tokens: [HEX, HEX]+
+    var values = tokens.map(token => token.value);
+    var codespaceranges = [];
+    for (var i = 0; i < values.length; i += 2) {
+      codespaceranges.push(values.slice(i, i + 2));
+    }
+    return Token('CODESPACERANGES', codespaceranges);
   }],
-];
-state_rules['BFCHAR'] = [
-  [/^endbfchar/, function(match) {
-    this.states.pop();
-    // return Token('END', 'CODESPACERANGE');
-    return null;
+  // the typical BFRANGE looks like "<0000> <005E> <0020>"
+  // the other kind of BFRANGE looks like "<005F> <0061> [<00660066> <00660069> <00660066006C>]"
+  ['BFRANGE', tokens => {
+    // tokens: [HEX, HEX, HEX | ARRAY<HEX>]+
+    var values = tokens.map(token => token.value);
+    var bfranges = [];
+    for (var i = 0; i < values.length; i += 3) {
+      bfranges.push(values.slice(i, i + 3));
+    }
+    return Token('BFRANGES', bfranges);
   }],
-  [/^\s+/, match => null ], // skip over whitespace
   // not sure how to parse a bfchar like this one:
-  // <0411><5168 fffd (fffd is repeated 32 times in total)>
-  [/^<([A-Za-z0-9]+)>\s*<([A-Za-z0-9 ]+)>/, match => {
-    // String.fromCharCode(parseInt('D840', 16), parseInt('DC3E', 16))
-    return Token('BFCHAR', {
-      from: parseInt(match[1], 16),
-      to: match[2].replace(/\s+/, ''),
-    });
+  //    <0411><5168 fffd (fffd is repeated 32 times in total)>
+  // String.fromCharCode(parseInt('D840', 16), parseInt('DC3E', 16))
+  ['BFCHAR', tokens => {
+    // tokens: [HEX, HEX]+
+    var values = tokens.map(token => token.value);
+    var bfchars = [];
+    for (var i = 0; i < values.length; i += 2) {
+      bfchars.push(values.slice(i, i + 2));
+    }
+    return Token('BFCHARS', bfchars);
   }],
 ];
 
 export class CMapParser {
-  tokenizer = new lexing.Tokenizer(default_rules, state_rules);
+  tokenizer = new lexing.Tokenizer(default_rules);
+  combiner = new lexing.Combiner(combiner_rules);
 
   /**
   Returns a mapping from in-PDF character codes to native Javascript Unicode strings
   */
   parse(iterable: lexing.StringIterable): string[] {
     var token_iterator = this.tokenizer.map(iterable);
+    var combined_iterator = this.combiner.map(token_iterator);
 
     var mapping: string[] = [];
 
+    var applyBFRangeOffset = (start: number, end: number, offset: number) => {
+      for (var i = 0; (start + i) <= end; i++) {
+        mapping[start + i] = mkString([offset + i]);
+      }
+    };
+    var applyBFRangeArray = (start: number, end: number, array: string[]) => {
+      for (var i = 0; (start + i) <= end; i++) {
+        mapping[start + i] = array[i];
+      }
+    };
+    var applyBFChar = (from: number, to: string) => {
+      mapping[from] = to;
+    };
+
     while (1) {
-      var token = token_iterator.next();
+      var token = combined_iterator.next();
+
       if (token.name === 'EOF') {
         break;
       }
-      // else if (token.name === 'CODESPACERANGE') {
-      //   for (var charCode = token.value.start; charCode <= token.value.end; charCode++) {
-      //     mapping[charCode] = String.fromCharCode(charCode);
-      //   }
-      // }
-      else if (token.name === 'BFRANGE') {
-        for (var i = 0; (token.value.start + i) <= token.value.end; i++) {
-          mapping[token.value.start + i] = String.fromCharCode(token.value.offset + i);
-        }
+      else if (token.name === 'CODESPACERANGES') {
+        token.value.forEach(tuple => {
+          var start = parseHex(tuple[0]);
+          var end = parseHex(tuple[1]);
+        });
       }
-      else if (token.name === 'BFCHAR') {
-        var charCodes = token.value.to.match(/.{4}/g).map(parseHex);
-        mapping[token.value.from] = String.fromCharCode.apply(null, charCodes);
+      else if (token.name === 'BFRANGES') {
+        token.value.forEach(triple => {
+          var start = parseHex(triple[0]);
+          var end = parseHex(triple[1]);
+          var arg = triple[2];
+          if (Array.isArray(arg)) {
+            var array = arg.map(item => parseHexstring(item, 4));
+            applyBFRangeArray(start, end, array);
+          }
+          else {
+            var offset = parseHex(arg);
+            applyBFRangeOffset(start, end, offset);
+          }
+        });
       }
-      // logger.info('%s => %j', token.name, token.value);
+      else if (token.name === 'BFCHARS') {
+        token.value.forEach(tuple => {
+          var from = parseHex(tuple[0]);
+          var to = parseHexstring(tuple[1], 4);
+          applyBFChar(from, to);
+        });
+      }
     }
 
     return mapping;
