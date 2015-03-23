0323c924b58f3de9bc14887a073627ebef28cba9
Christopher Brown <io@henrian.com>
2015-03-22 22:23:56 -0500

    Fix textparser tests

--- test/textparser.js
+++ test/textparser.js
@@ -1,18 +1,41 @@
 /// <reference path="../type_declarations/index.d.ts" />
 var assert = require('assert');
-var graphics = require('../parsers/graphics');
+var lexing = require('lexing');
+var models = require('../models');
+var drawing = require('../drawing');
+function createResources() {
+    var font_object = {
+        Type: "Font",
+        Subtype: "Type1",
+        Encoding: {
+            Type: "Encoding",
+            Differences: []
+        },
+        Widths: [],
+    };
+    var resource_object = {
+        Font: {
+            F10: font_object,
+        }
+    };
+    return new models.Resources(null, resource_object);
+}
+function renderString(contents) {
+    // var contents_string = this.joinContents('\n');
+    var iterable = new lexing.StringIterator(contents);
+    var resources = createResources();
+    var canvas = new drawing.Canvas([0, 0, 800, 600]);
+    canvas.render(iterable, resources);
+    return canvas.spans.map(function (span) { return span.text; });
+}
 describe('graphics text parsing', function () {
     it('should parse a simple text show operation', function () {
-        var canvas = new graphics.Canvas({});
-        canvas.renderString('BT (Adjustments must ) Tj ET');
-        var actual = canvas.spans.map(function (span) { return span.text; });
-        var expected = ['Adjustments must '];
+        var actual = renderString('/F10 11 Tf BT (Adjustments must) Tj ET');
+        var expected = ['Adjustments must'];
         assert.deepEqual(actual, expected);
     });
     it('should parse a nested string', function () {
-        var canvas = new graphics.Canvas({});
-        canvas.renderString('BT (In case of \\(dire\\) emergency) Tj ET');
-        var actual = canvas.spans.map(function (span) { return span.text; });
+        var actual = renderString('/F10 11 Tf BT (In case of \\(dire\\) emergency) Tj ET');
         var expected = ['In case of (dire) emergency'];
         assert.deepEqual(actual, expected);
     });
--- test/textparser.ts
+++ test/textparser.ts
@@ -1,24 +1,50 @@
 /// <reference path="../type_declarations/index.d.ts" />
 import assert = require('assert');
+import lexing = require('lexing');
 
-var graphics = require('../parsers/graphics');
+import models = require('../models');
+import graphics = require('../parsers/graphics');
+import drawing = require('../drawing');
+
+function createResources(): models.Resources {
+  var font_object = {
+    Type: "Font",
+    Subtype: "Type1",
+    Encoding: {
+      Type: "Encoding",
+      Differences: []
+    },
+    Widths: [],
+  };
+  var resource_object = {
+    Font: {
+      F10: font_object,
+    }
+  };
+  return new models.Resources(null, resource_object);
+}
+
+function renderString(contents: string): string[] {
+  // var contents_string = this.joinContents('\n');
+  var iterable = new lexing.StringIterator(contents);
+  var resources = createResources();
+
+  var canvas = new drawing.Canvas([0, 0, 800, 600]);
+  canvas.render(iterable, resources);
+
+  return canvas.spans.map(span => span.text);
+}
 
 describe('graphics text parsing', function() {
 
   it('should parse a simple text show operation', function() {
-    var canvas = new graphics.Canvas({});
-    canvas.renderString('BT (Adjustments must ) Tj ET');
-
-    var actual = canvas.spans.map(span => span.text);
-    var expected = ['Adjustments must '];
+    var actual = renderString('/F10 11 Tf BT (Adjustments must) Tj ET');
+    var expected = ['Adjustments must'];
     assert.deepEqual(actual, expected);
   });
 
   it('should parse a nested string', function() {
-    var canvas = new graphics.Canvas({});
-    canvas.renderString('BT (In case of \\(dire\\) emergency) Tj ET');
-
-    var actual = canvas.spans.map(span => span.text);
+    var actual = renderString('/F10 11 Tf BT (In case of \\(dire\\) emergency) Tj ET');
     var expected = ['In case of (dire) emergency'];
     assert.deepEqual(actual, expected);
   });
