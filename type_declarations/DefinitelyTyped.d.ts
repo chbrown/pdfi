983e2170064196adfd12bbb5a6c16e771fdad3c6
Christopher Brown <io@henrian.com>
2015-05-05 20:53:27 -0500

    Pull in unorm type declarations from my DefinitelyTyped fork

--- type_declarations/DefinitelyTyped.d.ts
+++ type_declarations/DefinitelyTyped.d.ts
@@ -3,4 +3,5 @@
 /// <reference path="DefinitelyTyped/lodash/lodash.d.ts" />
 /// <reference path="DefinitelyTyped/mocha/mocha.d.ts" />
 /// <reference path="DefinitelyTyped/node/node.d.ts" />
+/// <reference path="DefinitelyTyped/unorm/unorm.d.ts" />
 /// <reference path="DefinitelyTyped/yargs/yargs.d.ts" />
--- type_declarations/unorm.d.ts
+++
@@ -1,6 +0,0 @@
-declare module "unorm" {
-  function nfd(str: string): string;
-  function nfkd(str: string): string;
-  function nfc(str: string): string;
-  function nfkc(str: string): string;
-}
