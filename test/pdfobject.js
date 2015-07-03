8276fb045133fcbb46c5759d47b4743a6e3066ea
Christopher Brown <io@henrian.com>
2015-07-03 15:36:54 -0500

    Fix tests

--- test/pdfobject.js
+++ test/pdfobject.js
@@ -1,11 +1,14 @@
 /// <reference path="../type_declarations/index.d.ts" />
 var assert = require('assert');
 var PDF = require('../PDF');
-function check(input, expected_output, start) {
+var lexing_1 = require('lexing');
+var states_1 = require('../parsers/states');
+function check(input, expected) {
     var pdf = new PDF(null);
-    var output = pdf.parseString(input, start);
-    var message = "parse result does not match expected output.\n      parse(\"" + input + "\") => " + JSON.stringify(output) + "\n      but should == " + JSON.stringify(expected_output);
-    assert.deepEqual(output, expected_output, message);
+    var iterable = new lexing_1.StringIterator(input);
+    var output = new states_1.OBJECT(iterable, 1024).read();
+    var message = "parse result does not match expected output.\n      parse(\"" + input + "\") => " + JSON.stringify(output) + "\n      but should == " + JSON.stringify(expected);
+    assert.deepEqual(output, expected, message);
 }
 describe('pdfobject parser: general objects', function () {
     it('should parse short binary string', function () {
@@ -138,7 +141,7 @@ describe('pdfobject parser: general objects', function () {
                 Length: 81,
             },
         };
-        check(input, output, 'INDIRECT_OBJECT');
+        check(input, output);
     });
     it('should parse a list of booleans', function () {
         var input = "[true false true true ]";
@@ -154,125 +157,5 @@ describe('pdfobject parser: general objects', function () {
             buffer: new Buffer("hello there\ni am a stream"),
         };
         check(input, output);
-    });
-});
-describe('pdfobject parser: xref', function () {
-    it('short xref with trailing newline', function () {
-        var input = "xref\n0 2\n0000000000 65535 f\r\n0000000015 00000 n\r\n";
-        var output = [
-            {
-                object_number: 0,
-                offset: 0,
-                generation_number: 65535,
-                in_use: false
-            },
-            {
-                object_number: 1,
-                offset: 15,
-                generation_number: 0,
-                in_use: true
-            }
-        ];
-        check(input, output, 'XREF_ONLY');
-    });
-    it('another xref with trailing newline', function () {
-        var input = "xref\n100 3\n0000000197 00000 n\r\n0000000556 00000 n\r\n0001000023 00000 n\r\n";
-        var output = [
-            {
-                object_number: 100,
-                offset: 197,
-                generation_number: 0,
-                in_use: true
-            }, {
-                object_number: 101,
-                offset: 556,
-                generation_number: 0,
-                in_use: true
-            }, {
-                object_number: 102,
-                offset: 1000023,
-                generation_number: 0,
-                in_use: true
-            }
-        ];
-        check(input, output, 'XREF_ONLY');
-    });
-    it('xref from PDF32000_2008.pdf Section 7.5.4 EXAMPLE 2', function () {
-        var input = "xref\n0 6\n0000000003 65535 f\r\n0000000017 00000 n\r\n0000000081 00000 n\r\n0000000000 00007 f\r\n0000000331 00000 n\r\n0000000409 00000 n\r\n";
-        var output = [
-            {
-                "offset": 3,
-                "generation_number": 65535,
-                "in_use": false,
-                "object_number": 0
-            },
-            {
-                "offset": 17,
-                "generation_number": 0,
-                "in_use": true,
-                "object_number": 1
-            },
-            {
-                "offset": 81,
-                "generation_number": 0,
-                "in_use": true,
-                "object_number": 2
-            },
-            {
-                "offset": 0,
-                "generation_number": 7,
-                "in_use": false,
-                "object_number": 3
-            },
-            {
-                "offset": 331,
-                "generation_number": 0,
-                "in_use": true,
-                "object_number": 4
-            },
-            {
-                "offset": 409,
-                "generation_number": 0,
-                "in_use": true,
-                "object_number": 5
-            }
-        ];
-        check(input, output, 'XREF_ONLY');
-    });
-    it('xref from PDF32000_2008.pdf Section 7.5.4 EXAMPLE 3', function () {
-        var input = "xref\n0 1\n0000000000 65535 f\r\n3 1\n0000025325 00000 n\r\n23 2\n0000025518 00002 n\r\n0000025635 00000 n\r\n30 1\n0000025777 00000 n\r\n";
-        var output = [
-            {
-                "object_number": 0,
-                "offset": 0,
-                "generation_number": 65535,
-                "in_use": false
-            },
-            {
-                "object_number": 3,
-                "offset": 25325,
-                "generation_number": 0,
-                "in_use": true
-            },
-            {
-                "object_number": 23,
-                "offset": 25518,
-                "generation_number": 2,
-                "in_use": true
-            },
-            {
-                "object_number": 24,
-                "offset": 25635,
-                "generation_number": 0,
-                "in_use": true
-            },
-            {
-                "object_number": 30,
-                "offset": 25777,
-                "generation_number": 0,
-                "in_use": true
-            }
-        ];
-        check(input, output, 'XREF_ONLY');
     });
 });
--- test/pdfobject.ts
+++ test/pdfobject.ts
@@ -2,13 +2,17 @@
 import assert = require('assert');
 import PDF = require('../PDF');
 
-function check(input: string, expected_output: any, start?: string) {
+import {StringIterator} from 'lexing';
+import {OBJECT} from '../parsers/states';
+
+function check(input: string, expected: any) {
   var pdf = new PDF(null);
-  var output = pdf.parseString(input, start);
+  var iterable = new StringIterator(input);
+  var output =  new OBJECT(iterable, 1024).read();
   var message = `parse result does not match expected output.
       parse("${input}") => ${JSON.stringify(output)}
-      but should == ${JSON.stringify(expected_output)}`;
-  assert.deepEqual(output, expected_output, message);
+      but should == ${JSON.stringify(expected)}`;
+  assert.deepEqual(output, expected, message);
 }
 
 describe('pdfobject parser: general objects', () => {
@@ -173,7 +177,7 @@ endobj`;
         Length: 81,
       },
     };
-    check(input, output, 'INDIRECT_OBJECT');
+    check(input, output);
   });
 
   it('should parse a list of booleans', () => {
@@ -196,159 +200,6 @@ endstream`;
 i am a stream`),
     };
     check(input, output);
-  });
-
-});
-
-describe('pdfobject parser: xref', () => {
-
-  it('short xref with trailing newline', () => {
-    var input = `xref
-0 2
-0000000000 65535 f\r
-0000000015 00000 n\r
-`;
-    var output = [
-      {
-        object_number: 0,
-        offset: 0,
-        generation_number: 65535,
-        in_use: false
-      },
-      {
-        object_number: 1,
-        offset: 15,
-        generation_number: 0,
-        in_use: true
-      }
-    ];
-    check(input, output, 'XREF_ONLY');
-  });
-
-  it('another xref with trailing newline', () => {
-    var input = `xref
-100 3
-0000000197 00000 n\r
-0000000556 00000 n\r
-0001000023 00000 n\r
-`;
-    var output = [
-      {
-        object_number: 100,
-        offset: 197,
-        generation_number: 0,
-        in_use: true
-      }, {
-        object_number: 101,
-        offset: 556,
-        generation_number: 0,
-        in_use: true
-      }, {
-        object_number: 102,
-        offset: 1000023,
-        generation_number: 0,
-        in_use: true
-      }
-    ];
-    check(input, output, 'XREF_ONLY');
-  });
-
-  it('xref from PDF32000_2008.pdf Section 7.5.4 EXAMPLE 2', () => {
-    var input = `xref
-0 6
-0000000003 65535 f\r
-0000000017 00000 n\r
-0000000081 00000 n\r
-0000000000 00007 f\r
-0000000331 00000 n\r
-0000000409 00000 n\r
-`;
-    var output = [
-      {
-        "offset": 3,
-        "generation_number": 65535,
-        "in_use": false,
-        "object_number": 0
-      },
-      {
-        "offset": 17,
-        "generation_number": 0,
-        "in_use": true,
-        "object_number": 1
-      },
-      {
-        "offset": 81,
-        "generation_number": 0,
-        "in_use": true,
-        "object_number": 2
-      },
-      {
-        "offset": 0,
-        "generation_number": 7,
-        "in_use": false,
-        "object_number": 3
-      },
-      {
-        "offset": 331,
-        "generation_number": 0,
-        "in_use": true,
-        "object_number": 4
-      },
-      {
-        "offset": 409,
-        "generation_number": 0,
-        "in_use": true,
-        "object_number": 5
-      }
-    ];
-    check(input, output, 'XREF_ONLY');
-  });
-
-  it('xref from PDF32000_2008.pdf Section 7.5.4 EXAMPLE 3', () => {
-    var input = `xref
-0 1
-0000000000 65535 f\r
-3 1
-0000025325 00000 n\r
-23 2
-0000025518 00002 n\r
-0000025635 00000 n\r
-30 1
-0000025777 00000 n\r
-`;
-    var output = [
-      {
-        "object_number": 0,
-        "offset": 0,
-        "generation_number": 65535,
-        "in_use": false
-      },
-      {
-        "object_number": 3,
-        "offset": 25325,
-        "generation_number": 0,
-        "in_use": true
-      },
-      {
-        "object_number": 23,
-        "offset": 25518,
-        "generation_number": 2,
-        "in_use": true
-      },
-      {
-        "object_number": 24,
-        "offset": 25635,
-        "generation_number": 0,
-        "in_use": true
-      },
-      {
-        "object_number": 30,
-        "offset": 25777,
-        "generation_number": 0,
-        "in_use": true
-      }
-    ];
-    check(input, output, 'XREF_ONLY');
   });
 
 });
