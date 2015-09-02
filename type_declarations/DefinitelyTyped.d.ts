9f6175590814ac8c0ec279e8365890cd8881020a
Christopher Brown <io@henrian.com>
2015-09-02 15:32:38 -0500

    Consolidate type_declarations; remove lodash.d.ts

--- type_declarations/DefinitelyTyped.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference path="DefinitelyTyped/async/async.d.ts" />
-/// <reference path="DefinitelyTyped/chalk/chalk.d.ts" />
-/// <reference path="DefinitelyTyped/lodash/lodash.d.ts" />
-/// <reference path="DefinitelyTyped/mocha/mocha.d.ts" />
-/// <reference path="DefinitelyTyped/node/node.d.ts" />
-/// <reference path="DefinitelyTyped/unorm/unorm.d.ts" />
-/// <reference path="DefinitelyTyped/yargs/yargs.d.ts" />
--- type_declarations/index.d.ts
+++ type_declarations/index.d.ts
@@ -1,15 +1,21 @@
-/// <reference path="DefinitelyTyped.d.ts" />
+/// <reference path="DefinitelyTyped/async/async.d.ts" />
+/// <reference path="DefinitelyTyped/chalk/chalk.d.ts" />
+/// <reference path="DefinitelyTyped/mocha/mocha.d.ts" />
+/// <reference path="DefinitelyTyped/node/node.d.ts" />
+/// <reference path="DefinitelyTyped/unorm/unorm.d.ts" />
+/// <reference path="DefinitelyTyped/yargs/yargs.d.ts" />
 
 // self-declaring packages:
 /// <reference path="../node_modules/academia/academia.d.ts" />
 /// <reference path="../node_modules/adts/adts.d.ts" />
 /// <reference path="../node_modules/afm/afm.d.ts" />
+/// <reference path="../node_modules/arrays/arrays.d.ts" />
 /// <reference path="../node_modules/lexing/lexing.d.ts" />
 /// <reference path="../node_modules/unidata/unidata.d.ts" />
 /// <reference path="../node_modules/visible/visible.d.ts" />
-
-// my own external packages
-/// <reference path="loge.d.ts" />
+/// <reference path="../node_modules/loge/loge.d.ts" />
 
 // common global types
-/// <reference path="common.d.ts" />
+interface ErrorCallback { (err?: Error): void }
+interface StringMap { [index: string]: string; }
+interface ErrorResultCallback<T> { (err?: Error, result?: T): void }
