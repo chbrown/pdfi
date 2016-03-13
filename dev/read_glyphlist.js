bf1508cbadb267ec437a2fcbdbd61c59da4ed7ab
Christopher Brown <io@henrian.com>
2016-03-13 17:49:15 -0500

    Simplify read_glyphlist.js by using new dev helper module

--- dev/read_glyphlist.js
+++ dev/read_glyphlist.js
@@ -1,8 +1,16 @@
 /**
 Usage: `node read_glyphlist <glyphlist.txt >glyphlist.ts
 
-Where `glyphlist.txt` comes from http://partners.adobe.com/public/developer/en/opentype/glyphlist.txt
+Where `glyphlist.txt` comes from
+http://partners.adobe.com/public/developer/en/opentype/glyphlist.txt or similar,
+and has ;-separated lines like:
+
+nine;0039
+
+Where the first value is the glyphname, and the second value is the unicode
+character code.
 */
+var dev = require('./index');
 
 var chunks = [];
 process.stdin.on('readable', function() {
@@ -11,16 +19,11 @@ process.stdin.on('readable', function() {
 });
 process.stdin.on('end', function() {
   var glyphs = {};
-  Buffer.concat(chunks).toString('ascii').split(/\r?\n/).filter(function(line) {
-    var comment = line[0] === '#';
-    var empty = line.trim().length === 0;
-    return !comment && !empty;
-  }).forEach(function(line) {
-    var pair = line.split(';');
+  var glyphlist_data = Buffer.concat(chunks).toString('ascii');
+  dev.parseGlyphlist(glyphlist_data).map(function(pair) {
     var glyphname = pair[0];
     var alternatives = pair[1].split(',');
-    var charCodes = alternatives[0].split(' ').map(function(s) { return parseInt(s, 16); });
-    glyphs[glyphname] = String.fromCharCode.apply(null, charCodes);
+    glyphs[glyphname] = dev.parseUnicodeCharCodes(alternatives[0]);
   });
   process.stdout.write('export default ');
   process.stdout.write(JSON.stringify(glyphs, null, ' '));
